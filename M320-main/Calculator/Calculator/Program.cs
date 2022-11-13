4829159b0cd4ce2dbcc0386d14f28f6dce390dd9
Aleksej Markovic <112378800+aleksejmarkovic@users.noreply.github.com>
2022-11-13 22:42:21 +0100

    Add files via upload

---
+++ M320-main/Calculator/Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(;;)
+            {
+                int n1 = ReadInt("First number");
+                int n2 = ReadInt("Second number");
+                int n3 = ReadInt("Third number");
+
+                Console.WriteLine(Operations.Add(n1, n2, n3));
+                Console.WriteLine(Operations.Subtract(n1, n2, n3));
+                Console.WriteLine(Operations.Multiply(n1, n2, n3));
+                Console.WriteLine(Operations.Divide(n1, n2, n3));
+            }
+        }
+
+        static int ReadInt(string prefix)
+        {
+            Console.Write($"[PROMPT] :: {prefix} :: ");
+            int buffer;
+            if(int.TryParse(Console.ReadLine(), out buffer))
+            {
+                return buffer;
+            } else
+                Console.WriteLine($"[ERR] :: Must be a number. :: Try again.");
+
+            return ReadInt(prefix);
+        }
+    }
+}
---
+++ M320-main/Shapes/Shapes/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Shapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle("A rectangle", 10, 10, 4);
+            Rectangle r2 = new Rectangle(System.Drawing.Color.AliceBlue);
+            Rectangle r3 = new Rectangle(15, 10);   
+
+            r1.Enabled = true;
+            r1.Name = "My Rectangle";
+            
+            Console.WriteLine(
+                $"Rectangle: {r1.Name}\n" +
+                $"{r1.SizeAsString}"
+            );
+        }
+    }
+}
